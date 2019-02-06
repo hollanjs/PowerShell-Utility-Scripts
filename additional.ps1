d9d953581c4b5b603b7c44fda56cf4de94c3f8d3
hollanjs <hollanjs@users.noreply.github.com>
2019-02-06 10:51:26 -0500

    Added Elevated Console Check

--- additional.ps1
+++ additional.ps1
@@ -1,3 +1,11 @@
+# Ensure elevated console
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
 #region LOGGING
 $Global:ScriptLogFilePath = $pwd.Path + "\script log folder"
 if(!(Test-Path -Path $Global:ScriptLogFilePath))
